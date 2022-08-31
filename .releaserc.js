15b212d1132f7f6df7708803b9d55cfa96325297
wangxingkang <wang_xingkang@qq.com>
2022-08-31 20:47:07 +0800

    ci: add ci config

---
+++ .releaserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    "@semantic-release/release-notes-generator",
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
+      },
+    ],
+  ]
+}
