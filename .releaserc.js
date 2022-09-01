e06ca68906ff31d0005473b0453e49ad7033bb63
wangxingkang <wang_xingkang@qq.com>
2022-09-01 14:23:02 +0800

    chore: test

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  branches: [
+    'master', {name: 'beta', prerelease: true}
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
