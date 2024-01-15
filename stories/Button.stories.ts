ad34396195109436485817723d77ffebc61d738f
wangxingkang <wang_xingkang@qq.com>
2024-01-15 18:50:15 +0800

    chore: 文档迁移

---
+++ stories/Button.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Map } from '../src';
+
+import { Button } from './Button';
+
+const meta = {
+  title: '组件/Map',
+  component: Map,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Map>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
