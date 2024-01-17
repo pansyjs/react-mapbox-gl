7241f824aee1583d48a5213870ebe3e9faf341e8
wangxingkang <wang_xingkang@qq.com>
2024-01-17 22:12:48 +0800

    chore: 聚合点击实现

--- stories/examples/Cluster.stories.tsx
+++ stories/examples/Cluster.stories.tsx
@@ -35,6 +35,46 @@ const MarkerCluster = () => {
             'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
           },
         });
+
+        map.addLayer({
+          id: 'cluster-count',
+          type: 'symbol',
+          source: 'earthquakes',
+          filter: ['has', 'point_count'],
+          layout: {
+            'text-field': ['get', 'point_count_abbreviated'],
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12,
+          },
+        });
+
+        map.addLayer({
+          id: 'unclustered-point',
+          type: 'circle',
+          source: 'earthquakes',
+          filter: ['!', ['has', 'point_count']],
+          paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff',
+          },
+        });
+
+        map.on('click', 'clusters', (e) => {
+          const features = map.queryRenderedFeatures(e.point, {
+            layers: ['clusters'],
+          });
+          const clusterId = features[0].properties?.cluster_id;
+          map.getSource('earthquakes').getClusterExpansionZoom(clusterId, (err, zoom) => {
+            if (err) return;
+
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom,
+            });
+          });
+        });
       });
     }
   }, [map]);
