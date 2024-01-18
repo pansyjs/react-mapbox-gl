9ce8f77ba4ed99c04811d6a863c7e4f7936fdb16
wangxingkang <wang_xingkang@qq.com>
2024-01-18 13:36:05 +0800

    docs: 示例优化

--- stories/examples/Flyto.stories.tsx
+++ stories/examples/Flyto.stories.tsx
@@ -1,30 +1,51 @@
-import React, { useEffect } from 'react';
-import { Map, useMap } from '../../src';
+import React, { useEffect, useState } from 'react';
+import { Radio } from 'antd';
+import { Map, useMap, StyleLoadFinish } from '../../src';
 
 import type { Meta, StoryObj } from '@storybook/react';
+
+const themes = [
+  { label: 'dark', value: 'dark-v11' },
+  { label: 'satellite', value: 'satellite-streets-v12' },
+];
+
+const getStyleUrl = (value: string) => {
+  return `mapbox://styles/mapbox/${value}`;
+};
+
+const ThemeComponent = () => {
+  const { map } = useMap();
+  const [theme, setTheme] = useState(themes[0].value);
+
+  return (
+    <div style={{ position: 'absolute', background: '#efefef', right: 0, padding: 12 }}>
+      <Radio.Group
+        value={theme}
+        options={themes}
+        onChange={(e) => {
+          const value = e.target.value;
+          if (map) {
+            setTheme(value);
+            map.setStyle(getStyleUrl(value));
+          }
+        }}
+      />
+    </div>
+  );
+};
 
 const FlytoComponent = () => {
   const { map } = useMap();
 
   useEffect(() => {
-    let timeout: NodeJS.Timeout;
-
     if (map) {
-      map.once('style.load', () => {
-        timeout = setTimeout(() => {
-          map.flyTo({
-            center: [8.11862, 46.58842],
-            zoom: 12.5,
-            duration: 12 * 1000,
-            essential: true,
-          });
-        }, 2 * 1000);
+      map.flyTo({
+        center: [8.11862, 46.58842],
+        zoom: 12.5,
+        duration: 12 * 1000,
+        essential: true,
       });
     }
-
-    return () => {
-      timeout && clearTimeout(timeout);
-    };
   }, [map]);
 
   return null;
@@ -34,8 +55,17 @@ const meta = {
   title: '示例/Flyto',
   render: () => {
     return (
-      <Map containerStyle={{ height: '100vh' }} zoom={1} style="mapbox://styles/mapbox/dark-v11">
-        <FlytoComponent />
+      <Map containerStyle={{ height: '100vh' }} zoom={1} style={getStyleUrl(themes[0].value)}>
+        <StyleLoadFinish>
+          {(status, firstTime) => {
+            if (firstTime && status) {
+              return <FlytoComponent />;
+            }
+            return null;
+          }}
+        </StyleLoadFinish>
+
+        <ThemeComponent />
       </Map>
     );
   },
