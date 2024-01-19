ae4d069971fa6253c0ceecf0019f0729da34b38e
wangxingkang <wangxingkang@users.noreply.github.com>
2024-01-19 16:32:22 +0000

    deploy: 6ec86b96bc6feb58a6ca687285d41d975d98216d

---
+++ assets/Marker.stories-4yU0BPWa.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-5BUNAZ9W.js";import{M as n,S as i,a as c}from"./index-EmdqtECV.js";import"./index-4g5l5LRQ.js";import"./mapbox-gl-238DcKwu.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-im-bjvvC.js";const h={title:"组件/Marker",render:a=>r(n,{containerStyle:{height:500},children:r(i,{children:r(c,{...a,children:r("div",{style:{background:"red",borderRadius:"50%",padding:"3px"},children:"123"})})})}),parameters:{},tags:["autodocs"],argTypes:{},args:{lngLat:[-122.414,37.776]}},e={args:{onClick:a=>{console.log(a)}}};var o,s,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    onClick: e => {
+      console.log(e);
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const k=["Event"];export{e as Event,k as __namedExportsOrder,h as default};
