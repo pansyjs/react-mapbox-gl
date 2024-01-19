e15f0064689b52619f776c720c92cb16496468b7
wangxingkang <wangxingkang@users.noreply.github.com>
2024-01-19 11:24:19 +0000

    deploy: 02ff1d5f90c3b98b5b9d68ce448fb03c266391d3

---
+++ assets/Map.stories-JdscNzj-.js
@@ -0,0 +1,7 @@
+import{M as s}from"./index-084rKuYR.js";import"./index-4g5l5LRQ.js";import"./jsx-runtime-5BUNAZ9W.js";import"./mapbox-gl-238DcKwu.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-im-bjvvC.js";const g={title:"组件/Map",component:s,parameters:{},tags:["autodocs"],argTypes:{},args:{containerStyle:{height:500}}},o={args:{onClick:a=>{console.log(a)}}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    onClick: e => {
+      console.log(e);
+    }
+  }
+}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const d=["Event"];export{o as Event,d as __namedExportsOrder,g as default};
