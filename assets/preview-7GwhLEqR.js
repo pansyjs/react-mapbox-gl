bb5287820aac5aa21522cd6793c1c4c9159db23f
wangxingkang <wangxingkang@users.noreply.github.com>
2024-01-21 06:18:11 +0000

    deploy: 6365d9e03555b1d0c0133c134f7d6c7213ef8b62

--- assets/preview-7GwhLEqR.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-LNfjrUOB.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-eUnIw4RF.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-eUnIw4RF.js","./iframe-LNfjrUOB.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-2ligOMAm.js","./index-im-bjvvC.js","./index-ogXoivrg.js","./assertThisInitialized-WXViepx4.js","./_getPrototype-k2d-hbQX.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
