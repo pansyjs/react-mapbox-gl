bfe89b9cdaff500ecf6b64d2daa1a9e1cd313c98
wangxingkang <wangxingkang@users.noreply.github.com>
2024-01-21 05:46:42 +0000

    deploy: 9f459f228347692334b1bfbf829278f9c1127052

---
+++ assets/Marker.stories-u08a21ns.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-5BUNAZ9W.js";import{S as n}from"./index-tNdqmU4a.js";import{M as i}from"./marker-fnjo-1B5.js";import{M as c}from"./map-UcjS3gf5.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./mapbox-gl-0eWPQeFN.js";import"./index-im-bjvvC.js";const k={title:"组件/Marker",render:o=>r(c,{containerStyle:{height:500},children:r(n,{children:r(i,{...o,children:r("div",{style:{background:"red",borderRadius:"50%",padding:"3px"},children:"123"})})})}),parameters:{},tags:["autodocs"],argTypes:{},args:{lngLat:[-122.414,37.776]}},e={args:{onClick:o=>{console.log(o)}}};var a,t,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    onClick: e => {
+      console.log(e);
+    }
+  }
+}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const M=["Event"];export{e as Event,M as __namedExportsOrder,k as default};
