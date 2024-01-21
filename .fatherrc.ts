4f87ce9f973937b94c780e902dcbd7840a83bf7d
wangxingkang <wang_xingkang@qq.com>
2024-01-21 13:43:28 +0800

    chore: add release ci

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'redbud';
+import { defineConfig } from 'father';
 
 export default defineConfig({
   esm: {
     output: 'es',
+    transformer: 'esbuild',
   },
   cjs: {
     output: 'lib',
+    transformer: 'esbuild',
   },
   platform: 'browser',
 });
