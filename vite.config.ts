8320bf3355a199c2e0ab7f640a51b8556c387adb
wangxingkang <wang_xingkang@qq.com>
2024-01-24 18:28:12 +0800

    chore: 合并 vitest 配置至 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.mjs', '.ts', '.jsx', '.tsx', '.json'],
+  },
   plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+    css: true,
+  },
 });
