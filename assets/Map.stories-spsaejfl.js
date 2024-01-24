4d36e6fb7db7e9ff2f5948e17b5a6d5c4a5a9277
wangxingkang <wangxingkang@users.noreply.github.com>
2024-01-24 04:26:46 +0000

    deploy: 4e09144ac0fd7e87ca5eb79636937cf668a00db4

--- assets/Map.stories-spsaejfl.js
+++
@@ -1,7 +0,0 @@
-import{M as s}from"./map-UcjS3gf5.js";import"./jsx-runtime-5BUNAZ9W.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./mapbox-gl-0eWPQeFN.js";const l={title:"组件/Map",component:s,parameters:{},tags:["autodocs"],argTypes:{},args:{containerStyle:{height:500}}},o={args:{onClick:a=>{console.log(a)}}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    onClick: e => {
-      console.log(e);
-    }
-  }
-}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const g=["Event"];export{o as Event,g as __namedExportsOrder,l as default};
