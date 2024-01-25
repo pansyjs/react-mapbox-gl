6c0c3dfab628d5658808d31f25331bed9bd8bf85
wangxingkang <wangxingkang@users.noreply.github.com>
2024-01-25 02:29:47 +0000

    deploy: 45d215254c8d4ae439565754a2b800b6d2e2278e

--- assets/Map.stories-48208c8b.js
+++
@@ -1,7 +0,0 @@
-import{M as s}from"./map-66ebb850.js";import"./jsx-runtime-29545a09.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./mapbox-gl-801b965a.js";const l={title:"组件/Map",component:s,parameters:{},tags:["autodocs"],argTypes:{},args:{containerStyle:{height:500}}},o={args:{onClick:a=>{console.log(a)}}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    onClick: e => {
-      console.log(e);
-    }
-  }
-}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const g=["Event"];export{o as Event,g as __namedExportsOrder,l as default};
---
+++ assets/Marker.stories-fdffd6e5.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-29545a09.js";import{S as n}from"./index-3b053e66.js";import{M as i}from"./marker-6e013a53.js";import{M as c}from"./map-d3e02d13.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./mapbox-gl-801b965a.js";import"./index-da03a860.js";const k={title:"组件/Marker",render:o=>r(c,{containerStyle:{height:500},children:r(n,{children:r(i,{...o,children:r("div",{style:{background:"red",borderRadius:"50%",padding:"3px"},children:"123"})})})}),parameters:{},tags:["autodocs"],argTypes:{},args:{lngLat:[-122.414,37.776]}},e={args:{onClick:o=>{console.log(o)}}};var a,t,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    onClick: e => {
+      console.log(e);
+    }
+  }
+}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const M=["Event"];export{e as Event,M as __namedExportsOrder,k as default};
